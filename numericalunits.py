06b2fa45c7fab1e646c8cc0c3c0c0d95a9c69485
steve <steven.byrnes@gmail.com>
2026-05-15 20:57:35 -0400

    bump version number to 1.27 for new PyPI upload

--- numericalunits.py
+++ numericalunits.py
@@ -2,7 +2,7 @@
 """
 For information and usage see README, or https://github.com/sbyrnes321/numericalunits
 """
-# Copyright (C) 2012-2024 Steven J. Byrnes
+# Copyright (C) 2012-2026 Steven J. Byrnes
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -12,7 +12,7 @@ For information and usage see README, or https://github.com/sbyrnes321/numerical
 
 from math import pi
 
-__version__ = 1.26
+__version__ = 1.27
 
 
 ########## Set all variables, to help introspection libraries ################
